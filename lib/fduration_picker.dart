4fe2184c866ffbfe75560bdb347680ba6e53ac3e
dafinoer <dafinoer@outlook.com>
2022-08-06 16:06:58 +0700

    change widget type

--- lib/fduration_picker.dart
+++ lib/fduration_picker.dart
@@ -1,37 +1,41 @@
 import 'package:fduration/fduration_controller.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-class FDurationPicker extends StatefulWidget {
-  const FDurationPicker({
-    Key? key,
-    required this.durationMinute,
-    this.inFormat12Hour = false,
-  }) : super(key: key);
-
-  final int durationMinute;
-  final bool inFormat12Hour;
-
-  @override
-  State<FDurationPicker> createState() => _FDurationPickerState();
-}
-
-class _FDurationPickerState extends State<FDurationPicker> {
-  late final FDurationController _fDurationController;
-
-  @override
-  void initState() {
-    super.initState();
-    _fDurationController = FDurationController(durationController: 120);
-  }
-
-  @override
-  void dispose() {
-    _fDurationController.dispose();
-    super.dispose();
-  }
+class FDurationPickerWidget extends StatelessWidget {
+  const FDurationPickerWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(
+            child: PageView.builder(
+          itemCount: context
+              .select<FDurationController, List<int>>(
+                  (FDurationController value) => value.hours)
+              .length,
+          pageSnapping: false,
+          itemBuilder: (BuildContext context, int index) =>
+              Text('${context.read<FDurationController>().hours[index]}'),
+          onPageChanged: (int value) =>
+              context.read<FDurationController>().onSetHour(value),
+        )),
+        Expanded(
+          child: PageView.builder(
+            itemCount: context
+                .select<FDurationController, List<int>>(
+                    (FDurationController controller) => controller.minutes)
+                .length,
+            pageSnapping: false,
+            itemBuilder: (BuildContext context, int index) =>
+                Text('${context.read<FDurationController>().minutes[index]}'),
+            onPageChanged: (int value) =>
+                context.read<FDurationController>().onSetMinute(value),
+          ),
+        )
+      ],
+    );
   }
 }
