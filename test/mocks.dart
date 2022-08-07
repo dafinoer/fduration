8fa99842e3e0190f2c1b34da13f8df356f9174f8
dafinoer <dafinoer@outlook.com>
2022-08-07 22:50:18 +0700

    add stub class

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,52 +1,20 @@
-import 'dart:math';
-
 import 'package:fduration/fduration_controller.dart';
 import 'package:mocktail/mocktail.dart';
 
-class FDurationControllerMock extends Mock implements FDurationController {
-  int hour = 0;
-  int minute = 0;
+class FDurationControllerMock extends Mock implements FDurationController {}
 
-  List<int> listOfMinute(int minuteUnit) {
-    const int minute = 60;
-    final int minValueMinute = max(0, minuteUnit);
-    final List<int> minutes = <int>[];
-    int totalCurrent = 0;
-    while (totalCurrent < minute) {
-      minutes.add(totalCurrent);
-      final int multipleOfMinutes = minValueMinute % 60 != 0 ? minuteUnit : 60;
-      totalCurrent += multipleOfMinutes;
-    }
-    return minutes;
-  }
+class FDurationControllerStub extends FDurationController {
+  FDurationControllerStub(
+      {required super.durationController,
+      required super.dateNow,
+      super.multiplesOfMinutes});
 
-  @override
-  void onSetHour(int index) {
-    hour = hours[index];
-  }
-
-  @override
-  void onSetMinute(int index) {
-    minute = minutes[index];
-  }
-
-  @override
-  DateTime get value =>
-      DateTime(dateNow.year, dateNow.month, dateNow.day, hour, minute);
-
-  @override
-  List<int> onSetRemainTime() {
-    final int totalRemain = totalHourRemain;
-    final int hour = currentHour;
-    final List<int> hours = <int>[];
-    if (totalRemain > 1) {
-      hours.addAll(List<int>.generate(totalRemain, (int index) => hour + index)
-          .toList(growable: false));
-    } else if (totalRemain > 0) {
-      hours.addAll(<int>[hour, hour + 1]);
-    } else {
-      hours.add(hour);
-    }
-    return hours;
+  factory FDurationControllerStub.create(DateTime dateTime) {
+    const int hour = 3600000;
+    return FDurationControllerStub(
+      durationController: hour,
+      dateNow: dateTime,
+      multiplesOfMinutes: 15,
+    )..onSetTodayDateTime();
   }
 }
