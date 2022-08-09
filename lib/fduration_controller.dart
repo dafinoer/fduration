a3cd225dc72a72cded98e65cf21ea9c4dd228a95
dafinoer <dafinoer@outlook.com>
2022-08-09 22:20:25 +0700

    bugfix github action

--- lib/fduration_controller.dart
+++ lib/fduration_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
-import 'package:flutter/foundation.dart';
+
+import 'package:flutter/widgets.dart';
 
 class FDurationController extends ChangeNotifier {
   final DateTime dateNow;
